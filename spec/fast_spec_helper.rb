6edde694231c55b9775152a52d245583ca3f846a
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2015-02-12 16:10:34 -0500

    a little more cleanup

--- spec/fast_spec_helper.rb
+++ spec/fast_spec_helper.rb
@@ -10,14 +10,14 @@ Dir["spec/support/**/*.rb"].each { |f| require f }
 
 ENV["HOST"] = "test.host"
 ENV["SECRET_KEY_BASE"] = "test-key"
-ENV["HOUND_GITLAB_USERID"] = "1"
-ENV["HOUND_GITLAB_TOKEN"] = "houndgithubtoken"
+ENV["DOGE_GITLAB_USERID"] = "1"
+ENV["DOGE_GITLAB_TOKEN"] = "dogegithubtoken"
 ENV["ENABLE_HTTPS"] = "no"
 ENV["QUEUE"]= "low"
 ENV["GITLAB_ENDPOINT"]= "http://example.com/api/v3"
 ENV["CHANGED_FILES_THRESHOLD"] = "300"
 ENV["EXEMPT_ORGS"] = "thoughtbot,billybob"
-
+ENV["LOG_LEVEL"] = "debug"
 
 
 RSpec.configure do |config|
