80108db18ae551fa7c05512b5d28077bd6b3ba80
Steven Haddox <steven@haddox.us>
2015-02-20 09:05:53 -0500

    ActiveRecord DB session store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219193725) do
+ActiveRecord::Schema.define(version: 20150220140444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,16 @@ ActiveRecord::Schema.define(version: 20150219193725) do
 
   add_index "repos", ["active"], name: "index_repos_on_active", using: :btree
   add_index "repos", ["gitlab_id"], name: "index_repos_on_gitlab_id", using: :btree
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "users", force: true do |t|
     t.datetime "created_at",                       null: false
