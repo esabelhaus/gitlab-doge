48feb01f56c92c97e477be0275f6fcb38680d139
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2015-02-24 11:25:45 -0500

    wow, many monkey patch, such success, much repo syncronization

--- app/models/merge_request.rb
+++ app/models/merge_request.rb
@@ -68,10 +68,6 @@ class MergeRequest
     @api ||= Gitlab.client(
               :endpoint => ENV['GITLAB_ENDPOINT'],
               :private_token => ENV['DOGE_GITLAB_TOKEN']
-              #:httparty => {
-              #  :ssl_ca_file => ENV['SSL_CA_FILE_PATH'],
-              #  :pem => ENV['SSL_UNIFIED_CLIENT_CERT']
-              #}
             )
   end
 
