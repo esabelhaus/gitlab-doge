d02b8b29eaf85d6bea35721788627030d1e491f9
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2015-03-03 20:22:29 -0500

    fix specs for change to repo_sync_controller

--- spec/controllers/repo_syncs_controller_spec.rb
+++ spec/controllers/repo_syncs_controller_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe RepoSyncsController, '#create' do
-  it 'enqueues repo sync job' do
+  it 'enqueues repo sync job and sets refreshing_repos to true' do
     token = "usergitlabtoken"
     user = create(:user)
     stub_sign_in(user, token)
     allow(JobQueue).to receive(:push)
 
     post :create
+
+    expect(user.reload).to be_refreshing_repos
 
     expect(JobQueue).to have_received(:push).
       with(RepoSynchronizationJob, user.id, token)
--- spec/jobs/repo_synchronization_job_spec.rb
+++ spec/jobs/repo_synchronization_job_spec.rb
@@ -2,24 +2,13 @@ require 'spec_helper'
 require 'app/jobs/repo_synchronization_job'
 
 describe RepoSynchronizationJob do
-
-  describe '.before_enqueue' do
-    it 'sets refreshing_repos to true' do
-      user = create(:user)
-
-      RepoSynchronizationJob.new.before_enqueue(user.id, 'token')
-
-      expect(user.reload).to be_refreshing_repos
-    end
-  end
-
   describe '.perform' do
     it 'syncs repos and sets refreshing_repos to false' do
       user = create(:user, refreshing_repos: true)
       gitlab_token = 'token'
       synchronization = double(:repo_synchronization, start: nil)
       allow(RepoSynchronization).to receive(:new).and_return(synchronization)
-      
+
       RepoSynchronizationJob.new.perform(user.id, gitlab_token)
 
       expect(RepoSynchronization).to have_received(:new).with(
